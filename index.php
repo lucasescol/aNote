4d0076f9c55461923e007eea550309132629daee
Lucas Escolástico <lucasescolastico25@gmail.com>
2024-04-13 14:50:47 -0300

    :sparkles: feat: Criação da estrutura do site

--- index.php
+++ index.php
@@ -3,9 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <link rel="stylesheet" href="style.css">
     <title>aNote</title>
 </head>
 <body>
-    <h1>aNote</h1>
+    <header>
+        <h1>aNote</h1>
+    </header>
+
+    <main>
+        <div class="addTask">
+            <h2>Adicione Tasks</h2>
+            <form action="#">
+                <input type="text">
+                <input type="button" value="Adicionar">
+            </form>
+        </div>
+        <div class="taskList">
+            <h2>Lista de Task</h2>
+
+            <div class="task">
+                <div class="content">
+                    <i class="ph ph-circle"></i>
+                    <h3 class="title">Tarefa 1</h3>
+                </div>
+                <i class="ph-fill ph-trash" style="color: red;"></i>
+            </div>
+
+            <div class="task">
+                <div class="content">
+                    <i class="ph ph-circle"></i>
+                    <h3 class="title">Tarefa 2</h3>
+                </div>
+                <i class="ph-fill ph-trash" style="color: red;"></i>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
