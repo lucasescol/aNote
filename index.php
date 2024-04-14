130ba02d4c244cc014dd185837ae8c13630d4c26
Lucas Escolástico <lucasescolastico25@gmail.com>
2024-04-14 15:03:14 -0300

    :sparkles: feat: Exibição das tasks de forma dinâmica

--- index.php
+++ index.php
@@ -7,7 +7,14 @@
     <link rel="stylesheet" href="style.css">
     <title>aNote</title>
 </head>
-<body>
+<body>   
+    <?php
+        include("Connection.php");
+
+        $connection = new Connection();
+        $conn = $connection->connectDB();
+    ?>
+
     <header>
         <h1>aNote</h1>
     </header>
@@ -19,18 +26,11 @@
             <form method="POST">
                 <input type="text" name="task">
                 <input type="submit" value="Adicionar">
-
                 <?php
-                    include("Connection.php");
-
-                    $connection = new Connection();
-                    $conn = $connection->connectDB();
-                
                     //Create
                     if (isset($_POST['task'])){
                         $sql = $conn->prepare("INSERT into task VALUES (null, ?, 0)");
                         $sql->execute(array($_POST['task']));
-                        echo "Inserido com sucesso";
                     }
                 ?>
             </form>
@@ -39,21 +39,24 @@
         <div class="taskList">
             <h2>Lista de Task</h2>
 
-            <div class="task">
-                <div class="content">
-                    <i class="ph ph-circle"></i>
-                    <h3 class="title">Tarefa 1</h3>
-                </div>
-                <i class="ph-fill ph-trash" style="color: red;"></i>
-            </div>
+            <?php
+                $sql = $conn->prepare("SELECT * FROM task");
+                $sql->execute();
 
-            <div class="task">
-                <div class="content">
-                    <i class="ph ph-circle"></i>
-                    <h3 class="title">Tarefa 2</h3>
+                $fetchTasks = $sql->fetchAll();
+            ?>
+
+            <?php foreach ($fetchTasks as $task): ?>
+                <div class='task'>
+                    <div class='content'>
+                        <i class='ph ph-circle'></i>
+                        <h3 class='title'><?= $task['title'] ?></h3>
+                    </div>
+                    <a href="anote/delete" class="delete">
+                        <i class='ph-fill ph-trash'></i>
+                    </a>
                 </div>
-                <i class="ph-fill ph-trash" style="color: red;"></i>
-            </div>
+            <?php endforeach; ?>
         </div>
     </main>
 </body>
